0953b56da0cc2d8ebd506cabb748b5c0fbeb123d
a = 1 <juanchoquebaldiviezo@gmail.com>
2019-06-05 15:41:48 -0400

    feat: new test

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,16 @@
 class ImagesController < ApplicationController
     def new 
     end
+
+    def create
+        # render plain: params[:image].inspect
+        @image = Image.new image_params
+        @image.save
+    end
+
+
+    private 
+    def image_params
+        params.require(:image).permit(:description)
+    end
 end
